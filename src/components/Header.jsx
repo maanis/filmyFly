0aa3925defbe6244ddac7f09868cbd1b5ba54310
Manish Jha <manishjha962@gmail.com>
2025-01-28 20:45:39 +0530

    update title to 'FilmyFly' and enhance Header component styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div className='absolute w-32'>
+        <div className='absolute w-44 bg-gradient-to-br from-black/65 top-0 left-0'>
             <img src="https://mediaresource.sfo2.digitaloceanspaces.com/wp-content/uploads/2024/04/17002529/Filmy4wap-Logo-500x333.png" alt="" />
         </div>
     )
