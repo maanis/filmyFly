e220d0dca48c2b71986c879bd92083e09b3d2ff7
Manish Jha <manishjha962@gmail.com>
2025-01-28 20:19:54 +0530

    add routing with Login and Feed components; include axios and redux dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Login from './components/Login'
+import Feed from './components/Feed'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/feed" element={<Feed />} />
+    </Routes>
   )
 }
 
