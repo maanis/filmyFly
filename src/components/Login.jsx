e05212eb3aac0d1e902c9120d65974584efe5b6b
Manish Jha <manishjha962@gmail.com>
2025-01-29 10:58:56 +0530

    refactor Login component layout and enhance sign-in form with new styling and functionality

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,13 +6,18 @@ import Header from './Header'
 const Login = () => {
     return (
         <div style={{ backgroundImage: `url(${bg})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }} className='h-full flex justify-center items-center w-full relative'>
-            <div className="gradient absolute w-full h-full bg-black/55 "></div>
+            <div className="gradient absolute w-full h-full bg-black/35 "></div>
             <Header />
 
-            <div className='w-[25%] h-[70%] text-white rounded-md  bg-[#21212136] backdrop-blur-[8px] border border-red-600 relative p-5'>
+            <div className='w-[25%]  text-white rounded-md  bg-black/85 relative p-6'>
                 <h2 className='text-4xl font-semibold'>Sign In</h2>
-                <input type="text" placeholder='Email or mobile number' className='p-3 w-full text-lg outline-none border bg-transparent rounded-md border-zinc-800 mt-10' />
-                <input type="password" placeholder='Password' className='p-3 w-full text-lg outline-none border bg-transparent rounded-md border-zinc-800 mt-5' />
+                <form action="">
+                    <input type="text" placeholder='Email or mobile number' className='p-4 w-full text-lg outline-none border bg-transparent rounded-md border-zinc-400 mt-10' />
+                    <input type="password" autoComplete='true' placeholder='Password' className='p-4 w-full text-lg outline-none border bg-transparent rounded-md border-zinc-400 mt-5' />
+                    <button className='w-full cursor-pointer p-4 bg-red-600 rounded-md mt-8 text-2xl font-semibold'>Sign In</button>
+                    <Link className='w-full text-center mt-4 inline-block' to={'/'}>Forgot password?</Link>
+                    <p className='text-center mt-6 text-zinc-500'>New user? <span className='text-white cursor-pointer'>Sign up now</span></p>
+                </form>
             </div>
 
         </div>
