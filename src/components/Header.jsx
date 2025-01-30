846489673085f09bdc66ce669ca25dec77fd8d2a
Manish Jha <manishjha962@gmail.com>
2025-01-30 21:13:59 +0530

    style: adjust layout and styling of user profile image in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,8 +20,8 @@ const Header = ({ feed = false }) => {
             <div className='w-44  top-0 left-0'>
                 <img src="../../public/logo.png" alt="" />
             </div>
-            {feed && <div className='flex gap-4'>
-                <img src={data ? data.photoURL : 'https://occ-0-2087-2186.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABXz4LMjJFidX8MxhZ6qro8PBTjmHbxlaLAbk45W1DXbKsAIOwyHQPiMAuUnF1G24CLi7InJHK4Ge4jkXul1xIW49Dr5S7fc.png?r=e6e'} alt="" />
+            {feed && <div className='flex gap-2'>
+                <img className='w-8 rounded-full object-cover' src={data ? data.photoURL : 'https://occ-0-2087-2186.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABXz4LMjJFidX8MxhZ6qro8PBTjmHbxlaLAbk45W1DXbKsAIOwyHQPiMAuUnF1G24CLi7InJHK4Ge4jkXul1xIW49Dr5S7fc.png?r=e6e'} alt="" />
                 <button onClick={handleSignOut} className='font-semibold cursor-pointer text-red-600'>Logout</button>
             </div>}
         </div>
