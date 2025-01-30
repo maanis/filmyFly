44934b09be63e7ec0392eacffbb6db60db59743f
Manish Jha <manishjha962@gmail.com>
2025-01-30 20:03:13 +0530

    add Redux store and user slice for authentication state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Login from './components/Login'
 import Feed from './components/Feed'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './utils/firebase'
+import { useDispatch } from 'react-redux'
+import { addUser, removeUser } from './store/userSlice'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, displayName, email } = user;
+        dispatch(addUser(uid, displayName, email))
+      } else {
+        dispatch(removeUser())
+      }
+    });
+  }, [])
   return (
     <div className='h-screen w-full'>
       <Routes>
