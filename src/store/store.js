d983e9ec7bf7b2ea3892d7c3240e85a215a38af0
Manish Jha <143255115+maanis@users.noreply.github.com>
2025-02-02 20:41:22 +0530

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import userSlice from './userSlice'
 import movieSlice from './movieSlice'
 
+
 const store = configureStore({
     reducer: {
         user: userSlice,
