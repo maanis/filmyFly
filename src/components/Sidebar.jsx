9724da01c9338196fa03684564aca3a015bee558
Manish Jha <manishjha962@gmail.com>
2025-02-05 20:56:02 +0530

    feat: add remixicon stylesheet and enhance Sidebar component layout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Sidebar = () => {
     return (
-        <div className='w-[5%] h-full bg-red-700'>
-            Sidebar
+        <div className='w-[5%] h-full p-8 bg-red-700'>
+            <ul className='w-full flex flex-col items-center gap-5'>
+                {[<i class="ri-compass-line"></i>, <i class="ri-funds-line"></i>, <i class="ri-save-line"></i>, <i class="ri-account-circle-fill"></i>].map(e => (
+                    <li className='text-4xl cursor-pointer'>{e}</li>
+                ))}
+            </ul>
         </div>
     )
 }
