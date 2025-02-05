fe2758936c667f33022f7f7f5a261096cb8ecee0
Manish Jha <manishjha962@gmail.com>
2025-02-05 21:01:44 +0530

    fix: adjust padding and background color in Sidebar component for improved layout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Sidebar = () => {
     return (
-        <div className='w-[5%] h-full p-8 bg-red-700'>
-            <ul className='w-full flex flex-col items-center gap-5'>
+        <div className='w-[5%] h-full p-4 flex justify-center items-center bg-red-700'>
+            <ul className='w-full bg-yellow-600 h-full p-6 flex flex-col items-center gap-5'>
                 {[<i class="ri-compass-line"></i>, <i class="ri-funds-line"></i>, <i class="ri-save-line"></i>, <i class="ri-account-circle-fill"></i>].map(e => (
                     <li className='text-4xl cursor-pointer'>{e}</li>
                 ))}
