6bede0027e9d5a8f039560aa3da6210ee3d0ef27
Manish Jha <manishjha962@gmail.com>
2025-02-12 18:02:47 +0530

    feat: update Main component with improved select dropdown and styling enhancements

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,16 +10,17 @@ const Main = ({ sidebar, data, func }) => {
             <FeedHeader data={data} />
             <div className="flex pl-3 pr-5 justify-between items-center h-[6%]">
                 <p className='text-2xl font-semibold'>Popular</p>
-                <div class="select-dropdown">
-                    <select onChange={(e) => func(e.target.value)}>
+                <div class="select">
+                    <select id='format' name='format' onChange={(e) => func(e.target.value)}>
                         <option value="all">All</option>
                         <option value="movie">Movies</option>
                         <option value="tv">TV Shows</option>
                     </select>
                 </div>
             </div>
+
             <FeedCards />
-        </div>
+        </div >
     )
 }
 
