027eec8c2d3f23ba8777cdaa7b0536f3787e38cc
Manish Jha <manishjha962@gmail.com>
2025-02-13 21:37:52 +0530

    feat: enhance Loader component with loading message and timeout functionality

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import loader from '/loader.jpg'
 const Loader = () => {
+    const [text, settext] = useState('')
+    useEffect(() => {
+        setTimeout(() => {
+            settext("If it's not loading try using VPN...")
+            return () => {
+                settext('')
+            }
+        }, 6000);
+    }, [])
     return (
-        <div className='w-full flex bg-[#e2e2e2] justify-center items-center h-screen'>
+        <div className='w-full flex flex-col bg-[#e2e2e2] justify-center items-center h-screen'>
             <img style={{
                 filter: 'invert(1)'
             }} className='w-[32rem] object-cover' src={loader} alt="" />
+            {text && <h2 className='relative top-[-95px] text-red-500 font-semibold text-lg'>{text}</h2>}
         </div>
     )
 }
