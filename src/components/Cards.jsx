19356dce8aac2c3f0d9823d7183a700ac8a9cf71
Manish Jha <manishjha962@gmail.com>
2025-02-14 06:38:23 +0530

    feat: enhance Cards component with title display and gradient overlay; adjust vote average position

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { noImg } from '../utils/constants'
 
 const Cards = ({ data, title }) => {
+    const [showText, setshowText] = useState(false)
     return (
         <div className='w-full min-h-full overflow-y-auto overflow-x-hidden p-5 flex flex-wrap gap-5'>
             {data.map((d, i) => (
@@ -12,8 +13,11 @@ const Cards = ({ data, title }) => {
                         : noImg}')`,
                     backgroundSize: "cover",
                     backgroundPosition: "center",
-                }} key={i} className={`w-[13%] transition-all relative rounded-md cursor-pointer h-[16rem] inline-block shrink-0`}>
-                    {d.vote_average >= 0 && <div className="absolute bottom-8 right-[-15px] bg-yellow-500 rounded-full h-[45px] flex justify-center items-center font-semibold text-lg w-[45px]">{(d.vote_average * 10).toFixed()}<sup className='text-xs'>%</sup></div>}
+                }} key={i} className={`w-[13%]  transition-all relative rounded-md cursor-pointer h-[16rem] inline-block shrink-0`}>
+                    {d.vote_average >= 0 && <div className="absolute top-8 right-[-15px] bg-yellow-500 rounded-full h-[45px] z-50 flex justify-center items-center font-semibold text-lg w-[45px]">{(d.vote_average * 10).toFixed()}<sup className='text-xs'>%</sup>
+                    </div>}
+                    <div className="gradient z-10 absolute h-full w-full bg-gradient-to-t from-black rounded-md"></div>
+                    <h2 className={`z-50 ${showText ? 'visible' : 'hidden'} text-white absolute text-center w-full bottom-2 font-semibold`}>{d.title || d.name || d.original_name}</h2>
                 </Link>
             ))}
         </div>
