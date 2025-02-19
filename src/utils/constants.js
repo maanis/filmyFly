b90832ccd74a09cf39fe649eae080963a5910519
Manish Jha <manishjha962@gmail.com>
2025-02-19 12:54:56 +0530

    chore: update .gitignore to include .env; refactor OpenAI key to use environment variable

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,4 +4,4 @@ import img from '/noImg.jpg'
 export const logoImg = logo
 export const noImg = img
 
-export const openAi_key = 'AIzaSyDpP8Zi-23JiKnAUbzqJFL6IW1rdn0qBQs'
+export const openAi_key = import.meta.env.API_KEY;
